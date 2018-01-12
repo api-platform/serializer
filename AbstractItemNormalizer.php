jdeniau/DunglasApiBundle

Allow updating nested resource on POST
Fixes #1624, #1363, #427

src/Serializer/AbstractItemNormalizer.php
@@ -297,6 +297,7 @@ abstract class AbstractItemNormalizer extends AbstractObjectNormalizer
             ($propertyMetadata->isWritableLink() && \is_array($value))
         ) {
             $context['resource_class'] = $className;
+            $context['api_resource_is_relation'] = true;
 
             return $this->serializer->denormalize($value, $className, $format, $context);
         }

api-platform/serializer

AbstractItemNormalizer.php
@@ -297,6 +297,7 @@ abstract class AbstractItemNormalizer extends AbstractObjectNormalizer
             ($propertyMetadata->isWritableLink() && \is_array($value))
         ) {
             $context['resource_class'] = $className;
+            $context['api_allow_update'] = true;
 
             return $this->serializer->denormalize($value, $className, $format, $context);
         }